d2f134b9ba52c48be8a566aae9022e91e7f5bbca
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-07-13 22:27:43 +0800

    condense build script

--- ocd.sh
+++ ocd.sh
@@ -4,7 +4,7 @@ pushd opencv
 cmake . -Bbuild -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_opencv_apps=OFF
 cmake --build build --target install
 popd
-CXX_FLAGS="-I./opencv/build/install/include -L./opencv/build/install/x64/mingw/staticlib -lopencv_imgproc500 -lopencv_core500 -llibclapack"
-c++ -v -O3 -march=x86-64-v3 -s -c -o dct.o dct.cpp $CXX_FLAGS
-c++ -v -O3 -march=x86-64-v3 -s -c -o gaussianblur.o gaussianblur.cpp $CXX_FLAGS
-c++ -shared -v -O3 -march=x86-64-v3 -s -o vsocd.dll vsocd.cpp dct.o gaussianblur.o $CXX_FLAGS
+CXX_FLAGS="-I./opencv/build/install/include -L./opencv/build/install/x64/mingw/staticlib -lopencv_imgproc500 -lopencv_core500 -llibclapack -v -O3 -march=x86-64-v3 -s"
+c++ -c -o dct.o dct.cpp $CXX_FLAGS
+c++ -c -o gaussianblur.o gaussianblur.cpp $CXX_FLAGS
+c++ -shared -o vsocd.dll vsocd.cpp dct.o gaussianblur.o $CXX_FLAGS
