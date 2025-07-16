bfdd4d62e0a79aa2f550f407e8c662351af591d2
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-07-16 22:56:17 +0800

    im stupid
    "| 1" was written in wrong place (im not that stupid)
    but I do forgot to convert the whole thing to int LOL

--- source/gaussianblur.cpp
+++ source/gaussianblur.cpp
@@ -45,7 +45,7 @@ const VSFrame *VS_CC gblurGetFrame(int n, int activationReason, void *instanceDa
             }
 
             if (w != width || h != height) {
-                int sizeXS = (d->sizeX * ((double)w / width) | 1), sizeYS = (d->sizeY * ((double)h / height) | 1);
+                int sizeXS = int(d->sizeX * ((double)w / width)) | 1, sizeYS = int(d->sizeY * ((double)h / height)) | 1;
                 cv::GaussianBlur(inM, outM, cv::Size(sizeXS, sizeYS), d->sigmaX * ((double)w / width), d->sigmaY * ((double)h / height), d->borderType, cv::ALGO_HINT_ACCURATE);
             }
             else
